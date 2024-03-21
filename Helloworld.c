5c72170b9caafeb8a37d995380d0f648522c489f
Aromal-m-b <130879163+Aromal-m-b@users.noreply.github.com>
2024-03-21 08:26:33 +0530

    Create Helloworld.c
    Basic C program . Familirizing the printf() function

---
+++ Helloworld.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+//every program will have a main method the return type of the main mehtod can be changed as per the user's needs 
+//system starts the execution the program from the main method
+int main(){
+	//printf is a built in function used to print the result
+	printf("Hello world");
+}
